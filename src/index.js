939ebba0c1d773237366620cefd77e2a54afff09
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-02-14 21:41:41 +0100

    feat: WIP core logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,56 @@
-console.log('Hello World!')
+const debug = require('debug')('openssf-scorecard-monitor')
+const { getProjectScore, updateDatabase, saveScore, getScore, spliceIntoChunks } = require('./utils')
+const scope = require('../config/scope.json')
+const { maxRequestInParallel } = require('../config/settings.json')
+
+;(async () => {
+  const platform = 'github.com'
+  const projects = scope[platform]
+  console.log('projects', projects)
+  debug('Total projects in scope', projects.length)
+
+  const chunks = spliceIntoChunks(projects, maxRequestInParallel)
+  debug('Total chunks', chunks.length)
+
+  const scores = []
+  for (let index = 0; index < chunks.length; index++) {
+    const chunk = chunks[index]
+    debug('Processing chunk %s/%s', index + 1, chunks.length)
+
+    const chunkScores = await Promise.all(chunk.map(async ({ org, repo }) => {
+      // -- EACH REPO --
+      const { score, date } = await getProjectScore({ platform, org, repo })
+      console.log('Got project score for %s/%s/%s: %s (%s)', platform, org, repo, score, date)
+
+      const storedScore = getScore({ platform, org, repo })
+
+      const scoreData = { platform, org, repo, score, date }
+      // If no stored score then record if score is different then:
+      if (!storedScore || storedScore.score !== score) {
+        saveScore({ platform, org, repo, score, date })
+      }
+
+      // Add previous score and date if available to the report
+      if (storedScore) {
+        scoreData.prevScore = storedScore.score
+        scoreData.prevDate = storedScore.date
+
+        if (storedScore.score !== score) {
+          scoreData.currentDiff = parseFloat((score - storedScore.score).toFixed(1))
+        }
+      }
+
+      return scoreData
+    }))
+
+    scores.push(...chunkScores)
+  }
+
+  console.log('scores', scores)
+  // Generate the report
+
+  // Save database state
+  await updateDatabase()
+
+  // Commit the changes
+})()
