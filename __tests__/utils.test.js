a5d92f31c948ff931ed991983240ec835e5d2451
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2023-02-23 21:56:55 +0100

    test: for json schemas

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,5 +1,22 @@
+const { validateDatabaseIntegrity, validateScopeIntegrity } = require('../src/utils')
+const { database, scope } = require('../__fixtures__')
+
 describe('Utils', () => {
-  it('Should pass', () => {
-    expect(true).toBe(true)
+  describe('validateDatabaseIntegrity', () => {
+    it('Should manage a full database', () => {
+      expect(() => validateDatabaseIntegrity(database.fullDatabase)).not.toThrow()
+    })
+    it('Should manage an empty database', () => {
+      expect(() => validateDatabaseIntegrity(database.emptyDatabase)).not.toThrow()
+    })
+  })
+
+  describe('validateScopeIntegrity', () => {
+    it('Should manage a big Scope', () => {
+      expect(() => validateScopeIntegrity(scope.fullScope)).not.toThrow()
+    })
+    it('Should manage a empty Scope', () => {
+      expect(() => validateScopeIntegrity(scope.emptyScope)).not.toThrow()
+    })
   })
 })
