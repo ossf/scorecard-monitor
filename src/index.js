c53d679aa16223aa3dd4297319441e96053e95e0
Rob Bos <rajbos@users.noreply.github.com>
2023-03-29 15:04:35 +0000

    Enable support for OpenSSF Badges instead of only scores

--- src/index.js
+++ src/index.js
@@ -98,7 +98,7 @@ const generateScope = async ({ octokit, orgs, scope, maxRequestInParallel }) => 
   return newScope
 }
 
-const generateScores = async ({ scope, database: currentDatabase, maxRequestInParallel, reportTagsEnabled }) => {
+const generateScores = async ({ scope, database: currentDatabase, maxRequestInParallel, reportTagsEnabled, renderBadge }) => {
   // @TODO: Improve deep clone logic
   const database = JSON.parse(JSON.stringify(currentDatabase))
   const platform = 'github.com'
@@ -157,8 +157,8 @@ const generateScores = async ({ scope, database: currentDatabase, maxRequestInPa
 
   core.debug('All the scores are already collected')
 
-  const reportContent = await generateReportContent(scores, reportTagsEnabled)
-  const issueContent = await generateIssueContent(scores)
+  const reportContent = await generateReportContent(scores, reportTagsEnabled, renderBadge)
+  const issueContent = await generateIssueContent(scores, renderBadge)
 
   // SET OUTPUTS
   core.setOutput('scores', scores)
