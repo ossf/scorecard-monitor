2a3b60353f788b1ab4fe0840795ee74e2d7017a3
Sebastian Beltran <bjohansebas@gmail.com>
2025-02-25 22:52:14 -0500

    test: enhance input validation for GitHub token requirements in action tests
    Signed-off-by: Sebastian Beltran <bjohansebas@gmail.com>

--- __tests__/action.test.js
+++ __tests__/action.test.js
@@ -15,7 +15,7 @@ describe('action', () => {
     jest.clearAllMocks()
   })
 
-  it.each(["scope", "database", "report"])('should throw an error if the %s is not provided', async (input) => {
+  it.each(['scope', 'database', 'report'])('should throw an error if the %s is not provided', async (input) => {
     getInputMock.mockImplementation(name => {
       if (name === input) {
         throw new Error(`Input required and not supplied: ${input}`)
@@ -43,10 +43,26 @@ describe('action', () => {
     await main.run()
 
     expect(runMock).toHaveReturned()
-    expect(setFailedMock).toHaveBeenNthCalledWith(
+    expect(setFailedMock).toHaveBeenNthCalledWith(1,
       'The report-tool is not valid, please use: scorecard-visualizer, deps.dev'
     )
   })
 
   it.todo("should't throw an error if the available report is valid")
+
+  it.each(['auto-push', 'generate-issue', 'auto-commit', 'discovery-enabled'])('should throw an error if the github token is not provided when %s is enabled', async (input) => {
+    getInputMock.mockImplementation(name => {
+      if (name === input) {
+        return 'true'
+      }
+
+      return ''
+    })
+    await main.run()
+
+    expect(runMock).toHaveReturned()
+    expect(setFailedMock).toHaveBeenNthCalledWith(1,
+      'Github token is required for push, commit, create an issue and discovery operations!'
+    )
+  })
 })
